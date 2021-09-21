ed1108c784be5d477deb7217ab1b16f96e7cfc6b
van <van.nguyen35@gmail.com>
2021-09-21 11:45:05 +0100

    Add project files.

---
+++ todoplanner/Models/ToDoItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace todoplanner.Models
+{
+    public class ToDoItem
+    {
+        public Guid ID { get; set; }
+        public string Description { get; set; }
+        public bool IsComplete { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
